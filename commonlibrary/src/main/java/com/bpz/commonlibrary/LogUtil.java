97e3eee4fcd6a64892957e11e5262d69cee535a9
zyybp123 <bpzzr@outlook.com>
2018-05-18 18:03:20 +0800

    create project and add some configs

---
+++ commonlibrary/src/main/java/com/bpz/commonlibrary/LogUtil.java
@@ -0,0 +1,54 @@
+package com.bpz.commonlibrary;
+
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2018/5/18.
+ * 控制日志打印
+ */
+
+public class LogUtil {
+
+
+    public static final boolean IS_SHOW_LOG = BuildConfig.HAS_LOG;
+    private static String TAG = LogUtil.class.getSimpleName();
+
+    private LogUtil() {
+    }
+
+    public static void d(String tag, String msg) {
+        if (IS_SHOW_LOG) {
+            Log.d(tag, msg);
+        }
+    }
+
+    public static void e(String tag, String msg) {
+        if (IS_SHOW_LOG) {
+            Log.e(tag, msg);
+        }
+    }
+
+    public static void d(Object object, String msg) {
+        if (IS_SHOW_LOG) {
+            Log.d(object.getClass().getSimpleName(), msg);
+        }
+    }
+
+    public static void e(Object object, String msg) {
+        if (IS_SHOW_LOG) {
+            Log.e(object.getClass().getSimpleName(), msg);
+        }
+    }
+
+    public static void d(String msg) {
+        d(TAG, msg);
+    }
+
+    public static void e(String msg) {
+        e(TAG, msg);
+    }
+
+    public static String getLogTag(Object o) {
+        return o == null ? TAG : o.getClass().getSimpleName();
+    }
+}
